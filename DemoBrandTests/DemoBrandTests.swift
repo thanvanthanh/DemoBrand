7a1a09851ccd5566a93fc47805f5cc0d04f7f7c2
VanThanh <thanvanthanh1909@gmail.com>
2021-07-19 10:53:41 +0700

    update

---
+++ DemoBrandTests/DemoBrandTests.swift
@@ -0,0 +1,35 @@
+//
+//  DemoBrandTests.swift
+//  DemoBrandTests
+//
+//  Created by Thân Văn Thanh on 19/07/2021.
+//
+
+import XCTest
+
+class DemoBrandTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        func setUp(){
+            super.setUp()
+        }
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
