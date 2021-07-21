6732ca295718199eb1498ee0b1cd3ee2707f2fc0
VanThanh <thanvanthanh1909@gmail.com>
2021-07-21 07:46:04 +0700

    up

--- DemoBrand/Application/AppDelegate.swift
+++ DemoBrand/Application/AppDelegate.swift
@@ -6,18 +6,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        // khởi tạo cửa sổ
         window = UIWindow(frame: UIScreen.main.bounds)
-        
-        //  khởi tạo màn hình chính
+    
         let mainVC = BrandViewController()
         
         let navi = UINavigationController(rootViewController: mainVC)
-        // khởi tạo navigationController
-//        let mainNavigationController = UINavigationController(rootViewController: mainVC)
-        
-        // gán root ViewController
         window?.rootViewController = navi
         window?.makeKeyAndVisible()
         
